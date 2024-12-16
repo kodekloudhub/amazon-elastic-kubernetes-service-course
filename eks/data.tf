23c79718d19896a841bb297032406b6af8a4030a
fireflycons <34012094+fireflycons@users.noreply.github.com>
2024-12-16 20:42:46 +0000

    Update AMI

--- eks/data.tf
+++ eks/data.tf
@@ -38,5 +38,5 @@ data "aws_subnets" "public" {
 
 # Get AMI ID for latest recommended Amazon Linux 2 image
 data "aws_ssm_parameter" "node_ami" {
-  name = "/aws/service/eks/optimized-ami/1.29/amazon-linux-2/recommended/image_id"
+  name = "/aws/service/eks/optimized-ami/1.31/amazon-linux-2/recommended/image_id"
 }
