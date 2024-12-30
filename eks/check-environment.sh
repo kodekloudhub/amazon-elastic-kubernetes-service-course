c4cf218e2fbdfaa61ec07a05ef96381e6c690cae
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2024-12-30 07:36:26 +0000

    Constain cluster to AZ a, b and c

--- eks/check-environment.sh
+++ eks/check-environment.sh
@@ -7,16 +7,32 @@ NC="\e[0m"
 echo "Checking environment readiness to deploy a cluster"
 echo
 
-# Check enough subnets
-num_subnets=$(aws ec2 describe-subnets | jq '.Subnets | length')
+# Check we have required subnets
+# Fetch the list of availability zones
+available_zones=$(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output json)
 
-if [ $num_subnets -lt 3 ] ; then
-    echo -e "${RED}Insufficent subnets to deploy a cluster.${NC}"
+# Define the required suffixes
+required_suffixes=("a" "b" "c")
+
+# Initialize a flag to track missing zones
+missing_zones=()
+
+# Check for each required zone
+for suffix in "${required_suffixes[@]}"; do
+    if ! echo "$available_zones" | jq -e ".[] | select(endswith(\"$suffix\"))" &> /dev/null; then
+        missing_zones+=("$suffix")
+    fi
+done
+
+# If any required zones are missing, report error
+if [ ${#missing_zones[@]} -ne 0 ]; then
+    echo -e "${RED}Error: Missing availability zones with the following suffixes: ${missing_zones[*]}${NC}"
     echo "Please reset the lab and try again."
     echo "If the issue persists, raise a question on the forums."
     echo
 else
-    # Check for eksClusterRole being present
+    # We have the zones.
+    # Check for eksClusterRole being present and flag terraform accordingly.
     if aws iam get-role --role-name eksClusterRole > /dev/null 2>&1 ; then
         # Role is present, suppress generation by terraform
         export TF_VAR_use_predefined_role=true
